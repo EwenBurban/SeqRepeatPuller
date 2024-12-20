4fe03055a6e722c6fa6d09cfc6f5f6f0759fa584
Burban Ewen <ewen.burban@univ-rennes.fr>
2024-12-20 12:55:29 +0100

    alpha working version

--- setup.py
+++ setup.py
@@ -4,10 +4,16 @@ setup(
     name="snakemake_wrapper",
     version="1.0",
     packages=find_packages(),
+    package_data={
+        "snakemake_wrapper": [
+            "pipeline_*/*",   # Include pipeline files
+            "binpath_*/*",   # Include binpath scripts
+        ]
+    },
     entry_points={
         "console_scripts": [
-            "wrapper_tool1=snakemake_wrapper.wrapper_SeqRepeatPuller:main",
-            "wrapper_tool2=snakemake_wrapper.wrapper_get_WG_pos:main",
+            "SeqRepeatPuller=snakemake_wrapper.wrapper_SeqRepeatPuller:main",
+            "get_WG_pos=snakemake_wrapper.wrapper_get_WG_pos:main",
         ],
     },
     include_package_data=True,
